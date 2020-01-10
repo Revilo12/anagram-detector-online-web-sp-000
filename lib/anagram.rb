fd3bf2cf148cab1c0695e2b99a8d5ecf521fdeea
Oliver Dahl <oliverpdahl@gmail.com>
2020-01-10 12:59:43 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,17 @@
 # Your code goes here!
+class Anagram
+  attr_accessor :to_match
+
+  def initialize(to_match)
+    @to_match = to_match
+  end
+
+  def match(match_array)
+    #We want to find the anagrams of the to match
+    to_match = self.to_match
+    #We need to make sure that their length is the same as to match first (select)
+    same_length = match_array.select {|word| word.length == to_match.length}
+    puts same_length
+    #Each character in the first one needs to be present in the second
+
+end
